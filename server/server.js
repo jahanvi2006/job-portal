6a16c85f0ca67c0fb8c2f41da4720360f860fec1
Jahanvi Sharma <sharmajahanvi5023@gmail.com>
2025-09-24 18:46:11 +0530

    project completed

--- server/server.js
+++ server/server.js
@@ -4,20 +4,30 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
+import companyRoutes from './routes/companyRoutes.js'
+import connectCloudinary from './config/cloudinary.js'
+import jobRoutes from './routes/jobRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import {clerkMiddleware} from '@clerk/express'
 
 //initialise express
 const app = express()
 //connect db
 await connectDB()
+await connectCloudinary()
 
 //middlewares
 app.use(cors())
 app.use(express.json())
+app.use(clerkMiddleware())
 
 
 //routes
 app.get('/', (req, res)=> res.send("API Working"))
 app.post('/webhooks', clerkWebhooks)
+app.use('/api/company', companyRoutes)
+app.use('/api/jobs',jobRoutes)
+app.use('/api/users', userRoutes)
 
 
 
